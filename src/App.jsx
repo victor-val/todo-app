4a9f93e95160661549880c2f31df067670b8b7d2
victor-val <vval26v@gmail.com>
2023-02-25 14:46:40 +0100

    added input text inside of Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React, {useState} from 'react';
+import React, {Fragment, useState} from 'react';
 import { TodoList } from './TodoList';
 
 export function App(){
     const [todos, setTodos] = useState([
         {id: 1, task: 'tarea 1', completed: false}
     ]);
-    return <TodoList todos={todos}/>
+    
+    return (
+        <Fragment>
+            <TodoList todos={todos}/>
+            <input type="text" placeholder='Nueva tarea'></input>
+        </Fragment>
+
+    )
     
 }
