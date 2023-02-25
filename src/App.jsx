e0f1c3cacb285e9f174dfcbe0d438aae013fbc22
victor-val <vval26v@gmail.com>
2023-02-25 15:00:10 +0100

    add handle todo add with useRef and uuid

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useRef} from 'react';
+import {v4 as uuidv4} from 'uuid';
 import { TodoList } from './TodoList';
 
 export function App(){
     const [todos, setTodos] = useState([
         {id: 1, task: 'tarea 1', completed: false}
     ]);
+
+    const todoTaskRef = useRef()
+
+    const handleTodoAdd = () => {
+        const task = todoTaskRef.current.value;
+        if(task === '') return;
+        setTodos((prevTodos) => {
+            return [...prevTodos, {id: uuidv4(), task, completed: false}]
+        });
+    }
     
     return (
         <Fragment>
             <TodoList todos={todos}/>
-            <input type="text" placeholder='Nueva tarea'></input>
-            <button>Añadir</button>
+            <input ref={todoTaskRef} type="text" placeholder='Nueva tarea'></input>
+            <button onClick={handleTodoAdd}>Añadir</button>
             <button>Borrar</button>
         </Fragment>
     )
