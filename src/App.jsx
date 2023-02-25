7b72e37d9fdac2d5b3354f0cc8357d23cddcc0b8
victor-val <vval26v@gmail.com>
2023-02-25 14:44:03 +0100

    useState for todos list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TodoList } from './TodoList';
 
 export function App(){
-    return <TodoList todos={[{id: 1, task: 'tarea 1', completed: false}]}/>
+    const [todos, setTodos] = useState([
+        {id: 1, task: 'tarea 1', completed: false}
+    ]);
+    return <TodoList todos={todos}/>
     
 }
